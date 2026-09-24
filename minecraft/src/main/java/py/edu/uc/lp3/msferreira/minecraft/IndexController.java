6005b23041bef18bbdf1f8e0f27e1a780a10c971
MariaSol45 <ferreiramariasol2022@gmail.com>
2026-09-24 19:34:46 -0300

    fix: actualiza respuesta del IndexController

--- minecraft/src/main/java/py/edu/uc/lp3/msferreira/minecraft/IndexController.java
+++ minecraft/src/main/java/py/edu/uc/lp3/msferreira/minecraft/IndexController.java
@@ -1,13 +1,22 @@
 package py.edu.uc.lp3.msferreira.minecraft;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Hola Mundo";
-	}
+    @GetMapping("/")
+    public Map<String, String> index() {
+        Map<String, String> respuesta = new LinkedHashMap<>();
+
+        respuesta.put("estado", "API funcionando");
+        respuesta.put("dominio", "Minecraft");
+        respuesta.put("autora", "María Sol Ferreira");
+
+        return respuesta;
+    }
 }
